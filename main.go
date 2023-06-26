3910b062e45954bf445e6a9a2dbec79684359633
sunny <yshee712@gmail.com>
2023-06-26 09:43:37 +0900

    #17 SQS로 메시지 보내기

--- main.go
+++ main.go
@@ -16,15 +16,17 @@ func main() {
 	}))
 
 	//queueURL := ""
-	SNSURL := ""
-
-	err := send.SendSNSMessage(SNSURL, sess)
+	SQSURL := "https://sqs.ap-northeast-2.amazonaws.com/400281609678/SQS_TEST2.fifo"
+	eventType := "donationRegistered"
+	message := map[string]interface{}{
+		"donationId": 1,
+		"memberId":   1,
+		"mobile":     "01000000000",
+		"content":    "메시지 내용입니다.",
+	}
+	err := send.SendSqsMessage(sess, &SQSURL, eventType, message)
 	if err != nil {
 		log.Println("메시지 전송 에러")
 	}
 
-	//err := send.SendSqsMessage(sess, &queueURL, "emil", "메시지 내용")
-	//if err != nil {
-	//	log.Println("메시지 전송 에러")
-	//}
 }
